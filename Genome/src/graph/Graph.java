2011261985dc8ed6b38d9b3a6733de7c8469163a
Andreas Timoudas <andreas@Andreass-Air.lan>
2020-05-11 14:23:45 +0200

    log update and start of parseing funtions to load the data into a graph datastructure

--- Genome/src/graph/Graph.java
+++ Genome/src/graph/Graph.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import static java.lang.System.out;
 
 
 /*
@@ -14,33 +15,66 @@ import java.util.*;
 
 public class Graph {
 	String row;
+	static int _vertices;
+	ArrayList<ArrayList<Integer>> adj_list;
 	
-	public Graph(string filename) {
+	public Graph() {
 		
 	}
 	
 	/*
 	 * @param filename
+	 * Load the identifiers of the vertices into an ArrayList
 	 */
-	private ArrayList<Double> ParseData(String filename) {
+	private ArrayList<String> LoadIdentifiers(String filename) {
+		ArrayList<String> Identifiers = new ArrayList<String>();
+		String[] data = null;
 		try {
 			String path = filename;
 			BufferedReader Genomefile = new BufferedReader(new FileReader(path));
 			while ((row = Genomefile.readLine()) != null) {
-			    String[] data = row.split(",");
-			    System.out.println("Node: " + data[0] + "," +" "+ data[1]);
-			    //Check for redundancy 
-			    //ParseIdentifers here
+			    data = row.split("\t");
+			    if (!Identifiers.contains(data[0])) {
+			    	Identifiers.add(data[0]);
+			    }
+			    if (!Identifiers.contains(data[1])) {
+			    	Identifiers.add(data[1]);
+			    }
 			}
-			graphFile.close();
+			Genomefile.close();
 		
 		} catch(IOException ex){
 		System.out.println(ex);
 		}
+		return Identifiers;
 	}
 	
-	private void ParseIdentifers() {
+	public ArrayList<ArrayList<Integer>> ParseData() {
+		adj_list = new ArrayList<ArrayList<Integer>>(_vertices);
 		
 	}
+	
+	/*
+	 * Parse the Identiers into unique integers and map them in a HashMap
+	 */
+	private HashMap<String, Integer> ParseIdentifers(ArrayList<String> Identifiers) {
+		HashMap<String, Integer> IDs = new HashMap<String, Integer>();
+		int IntID = 0;
+		for (String id : Identifiers) {
+			IDs.put(id, IntID);
+			IntID++;
+		}
+		_vertices = IntID;
+		return IDs;
+	}
+	
+	public static void main(String[] args) {
+		Graph graph = new Graph();
+		String[] data = graph.ParseData("/Users/andreas/eclipse-workspace/Genome/data/test.txt");
+		for(String s: data) {
+			out.println(s);
+		}
+	}
+		
 
 }
